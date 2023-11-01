7ed58ca79f684367e5b4bb97395978bc3caf4278
Samuel Bartolomedi <samuelbartolomedi1994@gmail.com>
2023-11-01 13:58:01 -0300

    commit final

--- Desafio01Cap08/src/main/java/com/devsuperior/Desafio01Cap08/services/OrderService.java
+++ Desafio01Cap08/src/main/java/com/devsuperior/Desafio01Cap08/services/OrderService.java
@@ -1,11 +1,20 @@
 package com.devsuperior.Desafio01Cap08.services;
 
 import com.devsuperior.Desafio01Cap08.entities.Order;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderService {
+
+    @Autowired
+    private ShippingService shippingService;
+
+    public OrderService(ShippingService shippingService) {
+        this.shippingService = shippingService;
+    }
+
     public Double total(Order order) {
-        return order.getBasic()
+        return order.getBasic() - (order.getDiscount() / 100 * order.getBasic()) + shippingService.shipment(order);
     }
 }
