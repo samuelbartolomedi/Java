1b86671e09fb8dbdfee134aced7f9ca361ce85ca
Samuel Bartolomedi <samuelbartolomedi1994@gmail.com>
2024-02-16 17:27:57 -0300

    adding class category, join table product/category

--- ExeAula05Cap08/src/main/java/com/devsuperior/dscommerce/entities/User.java
+++ ExeAula05Cap08/src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -12,7 +12,7 @@ import java.util.List;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
     private String name;
     private String email;
     private String phone;
