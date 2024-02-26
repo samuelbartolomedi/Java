dfb839ffc633f571949b6d963a1108683bf4edb7
Samuel Bartolomedi <samuelbartolomedi1994@gmail.com>
2024-02-26 13:15:05 -0300

    criação do repo do desafio modelo de dominio e orm cap 08

---
+++ Desafio02Cap08/src/main/java/com/devsuperior/Desafio02Cap08/Desafio02Cap08Application.java
@@ -0,0 +1,13 @@
+package com.devsuperior.Desafio02Cap08;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Desafio02Cap08Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Desafio02Cap08Application.class, args);
+	}
+
+}
