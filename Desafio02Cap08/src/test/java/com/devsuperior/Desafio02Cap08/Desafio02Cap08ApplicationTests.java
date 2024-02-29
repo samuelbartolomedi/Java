a325d1c494802f6c0f5333a1b133980c4bd91af0
Samuel Bartolomedi <samuelbartolomedi1994@gmail.com>
2024-02-29 15:32:38 -0300

    recriando projeto

---
+++ Desafio02Cap08/src/test/java/com/devsuperior/Desafio02Cap08/Desafio02Cap08ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.Desafio02Cap08;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Desafio02Cap08ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
